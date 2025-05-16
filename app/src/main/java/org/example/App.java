eb88cc012c9d50105f3821f50ceda711369513a7
Abhishek Ruhela <94753084+mysterioxx@users.noreply.github.com>
2025-05-16 14:34:09 +0530

    classes
    ticket and user class

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -4,12 +4,7 @@
 package org.example;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
-
     public static void main(String[] args) {
 
-        System.out.println(new App().getGreeting());
     }
 }
---
+++ app/src/main/java/org/example/entities/user.java
@@ -0,0 +1,9 @@
+package org.example.entities;
+
+public class user {
+    private String name;
+    private String password;
+    private String hashpassword;
+    private List<Ticket>  ticketBooked;
+    private String userId;
+}
